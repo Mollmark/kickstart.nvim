af4bd4685a76be08d5947907480576703f1d4970
permollmark <pm@bsr.se>
2024-04-05 13:45:40 +0200

    Neo tree and git
    Neo tree instead of nvim tree
    Lazygit plugin
    Keymaps in separate file

--- lua/custom/keymaps/keymaps.lua
+++ lua/custom/keymaps/keymaps.lua
@@ -1,3 +1,5 @@
 return {
   vim.keymap.set('n', '<leader>bd', ':<C-U>bprevious <bar> bdelete #<CR>', { desc = 'Close buffer' }),
+  vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { silent = true, desc = 'Toggle Neotree filesystem' }),
+  vim.keymap.set('n', '<leader>ww', '<C-w>', { desc = 'Window comands' }),
 }
