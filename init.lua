8f5c591bd27b46eb7dfb079bfefea047438b4c44
permollmark <pm@bsr.se>
2024-06-28 10:46:33 +0200

    plugins
    some more plugins and gruvbox

--- init.lua
+++ init.lua
@@ -700,7 +700,7 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<yy>'] = cmp.mapping.confirm { select = true },
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
@@ -744,16 +744,16 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     --'folke/tokyonight.nvim',
-    -- 'sainnhe/gruvbox-material',
+    'sainnhe/gruvbox-material',
     --'diegoulloao/neofusion.nvim',
-    'catppuccin/nvim',
-    name = 'catppuccin',
+    --'catppuccin/nvim',
+    name = 'gruvbox-material',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin'
+      vim.cmd.colorscheme 'gruvbox-material'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
