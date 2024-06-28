819ede712f77c6f027e2a5e253cd5b3d02d92067
permollmark <pm@bsr.se>
2024-06-28 11:10:05 +0200

    wilder
    Wildermenu

--- lua/custom/plugins/wildmenu.lua
+++ lua/custom/plugins/wildmenu.lua
@@ -1,6 +1,15 @@
 return {
   'gelguy/wilder.nvim',
   config = function()
-    -- config goes here
+    local wilder = require 'wilder'
+    wilder.setup {
+      modes = { ':', '/', '?' },
+    }
+    wilder.set_option(
+      'renderer',
+      wilder.popupmenu_renderer {
+        pumblend = 20,
+      }
+    )
   end,
 }
